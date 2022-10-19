c3c3e9411204ad073669e31556abb0c47868a7fd
kaviyasokkalingam <116163418+kaviyasokkalingam@users.noreply.github.com>
2022-10-19 21:08:04 +0530

    Add files via upload

---
+++ BookServiceInterface.java
@@ -0,0 +1,12 @@
+
+public interface BookServiceInterface {
+    void addBook();
+
+    void showAllBooks();
+
+    void showAllAvailableBooks();
+
+    void borrowBook();
+
+    void returnBook();
+}
